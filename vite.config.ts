5919d095a385c84d320e2981dbb1af26e7a97483
TiagoLeopoldo <tnleopoldo.dev@gmail.com>
2025-10-18 00:42:42 -0300

    feat: adiciona estrutura de logo, busca e botões de filtro e seus estilos responsivos

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    watch: {
+      usePolling: true, // 👈 força o Vite a detectar mudanças em sistemas de arquivos instáveis
+    },
+  },
+});
