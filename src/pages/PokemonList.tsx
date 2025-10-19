6e4a282de224edbaaf3c1c751bb669cf252239bf
TiagoLeopoldo <tnleopoldo.dev@gmail.com>
2025-10-19 14:34:24 -0300

    feat: filtragem por tipo aplicada aos Pokémon da página atual

--- src/pages/PokemonList.tsx
+++ src/pages/PokemonList.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import './PokemonList.css';
 import PokemonCard from '../components/PokemonCard';
-import { getPokemon, pokemonDetails, getPokemonByType } from '../services/pokedex.service.ts';
+import { getPokemon, pokemonDetails } from '../services/pokedex.service.ts';
 import type { Pokemon, PokemonsCardData } from '../types/types.ts';
 
 interface PokemonListProps {
@@ -9,7 +9,7 @@ interface PokemonListProps {
 }
 
 
-const PokemonList = ( { selectedType }: PokemonListProps) => {
+const PokemonList = ({ selectedType }: PokemonListProps) => {
   const [pokemons, setPokemons] = useState<PokemonsCardData[]>([]);
   const [offset, setOffset] = useState<number>(0);
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -18,24 +18,8 @@ const PokemonList = ( { selectedType }: PokemonListProps) => {
   useEffect(() => {
     const loadInitialData = async () => {
       setIsLoading(true);
-
-      if (selectedType) {
-        const typePokemons = await getPokemonByType(selectedType);
-        const detailed = await Promise.all(
-          typePokemons.map(async (pokemon: Pokemon) => {
-            const details = await pokemonDetails(pokemon.name);
-            return {
-              id: details.id,
-              name: details.name,
-              image: details.sprites.front_default,
-              type: details.types[0].type.name,
-            };
-          })
-        );
-        setPokemons(detailed);
-      } else {
       const data = await getPokemon(20, offset);
-      const detaleidPokemons = await Promise.all(
+      const detailed = await Promise.all(
         data.results.map(async (pokemon: Pokemon) => {
           const details = await pokemonDetails(pokemon.name);
           return {
@@ -46,18 +30,21 @@ const PokemonList = ( { selectedType }: PokemonListProps) => {
           };
         })
       );
-      setPokemons(detaleidPokemons);
-    }
-          setIsLoading(false);
+      setPokemons(detailed);
+      setIsLoading(false);
     };
     loadInitialData();
-  }, [offset, selectedType])
+  }, [offset]);
+
+  const filteredPokemons = selectedType
+  ? pokemons.filter((pokemon) => pokemon.type === selectedType)
+  : pokemons;
 
   return (
     <>
       {isLoading ? <h1>Carregando...</h1> : (
         <section className='section-pokemon-list'>
-          {pokemons.map((pokemon: PokemonsCardData) => (
+          {filteredPokemons.map((pokemon: PokemonsCardData) => (
             <PokemonCard {...pokemon} />
           ))}
         </section>
